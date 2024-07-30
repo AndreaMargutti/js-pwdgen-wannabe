3be7b5b0b02a6404e1f91e41bb24ab73ab741c38
AndreaMargutti <marguttiandrea30@gmail.com>
2024-07-30 15:31:59 +0200

    scaletta + fase di preparazione

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-console.log('JS OK');
+/* Scaletta Mentale
+1. Recuperare il div html su cui andrà stampato l'output
+2. Creare la variabile con all'interno il numbero 21
+2. Chiedere il nome all'utente
+3. Chiedere il cognome all'utente
+4. Chiedere il colore preferito all'utente
+5. Sommare nome, congome e colore e 21
+6. Stampare in console la somma sopra eseguita.
+7. Stampare in pagine la somma del punto 6
+*/
+
+// # Fase di preparazione
+// creo variabile con numero 21 
+const passwordNumber = 21;
+console.log(21, typeof passwordNumber);
+
+//recupero il div dall'html
+const myDiv = document.getElementById('my-div')
+console.log(myDiv)
+
+// # Fase di raccolta dati
+
+// # Fase di elaborazione dati
+
+//# Fase di produzione output
